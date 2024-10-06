06795efb8dd6c2978f16f77af0fad198d8ea98e2
sebastiagulli <sebastiangulli@outlook.com>
2024-10-06 19:15:29 -0300

    create header structure, icon component, add horizontal logo and apply styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import '../public/styles/main.css'
 import '../public/styles/font.css'
 import '../public/styles/classes.css'
 
+import { Header } from './components/Header'
+
 export const App = () => {
     return (
         <>
-            
+            <Header />
         </>
     )
 }
---
+++ src/components/Icon.jsx
@@ -0,0 +1,10 @@
+export const Icon = ({className, children}) => {
+    return (
+        <svg className={className}
+        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+        stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            {children}
+        </svg>
+    )
+}
