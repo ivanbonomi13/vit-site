a82c4b9bb095d73d51d85a989d33279d5b4f5a21
sebastiangulli <sebastiangulli@outlook.com>
2024-10-10 11:29:33 -0300

    fix some bg styles

--- src/components/HeroBackground.jsx
+++ src/components/HeroBackground.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { Icon } from "./Icon"
 import '../../public/styles/components/herobg.css'
 
@@ -52,11 +53,13 @@ const icons = [
 ]
 
 export const HeroBackground = () => {
+    const [hideLines, setHideLines] = useState(false)
+    useEffect(() => setHideLines(true), [])
     return (
         <div className="hero__container__bg hero-bg">
             {
                 icons.map((icon, index) =>
-                    <div key={index} className="hero__container__bg__box hero-bg-box">
+                    <div key={index} className={`hero__container__bg__box hero-bg-box${!hideLines ? ' bg-hidden-lines' : ''}`}>
                         <Icon className={'hero__container__bg__box__icon hero-bg-icon'}>
                             {icon}
                         </Icon>
