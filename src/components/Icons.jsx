013dc9cd5e427d95226ea0523ff730ee972b7903
Sebastián Gulli <sebastiangulli@outlook.com>
2024-10-25 15:33:23 -0300

    optimize footer

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -500,8 +500,23 @@ const icons = [
                 ]
             }
         ]
+    },
+    {
+        'section': 'footer',
+        'list': [
+            {
+                'name': 'ubication',
+                'icon':
+                    <>
+                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                        <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                    </>
+            }
+        ]
     }
 ]
+
+// FUNTION TO GET ICONS
 
 export const getIcons = (section, part, name) => {
     const sectionIcons = icons.find(item => item.section === section)
--- src/components/site/Footer.jsx
+++ src/components/site/Footer.jsx
@@ -1,4 +1,6 @@
 import '../../../public/styles/components/footer.css'
+
+import { getIcons } from '../Icons'
 import { Icon } from '../Icon'
 
 const sections = [
@@ -82,14 +84,14 @@ export const Footer = () => {
                 <div className='footer__container__grid footer-grid'>
                     {
                         sections.map((section, index) =>
-                            <div className='footer__container__grid__section footer-grid-section' key={index}>
-                                <h4 className='footer__container__grid__section__title footer-grid-section-title'>
+                            <div className='footer__container__grid__section footer-section' key={index}>
+                                <h4 className='footer__container__grid__section__title footer-section-title'>
                                     {section.title}
                                 </h4>
-                                <ul className='footer__container__grid__section__content footer-grid-section-content'>
+                                <ul className='footer__container__grid__section__content footer-section-content'>
                                     {section.content.map((item, index) =>
-                                        <li className='footer__container__grid__section__content__item footer-grid-section-content-item' key={index}>
-                                            <a className='footer__container__grid__section__content__item__link footer-grid-section-content-item-link' href={`#${item.id}`}>
+                                        <li className='footer__container__grid__section__content__item footer-section-item' key={index}>
+                                            <a className='footer__container__grid__section__content__item__link footer-section-link' href={`#${item.id}`}>
                                                 {item.name}
                                             </a>
                                         </li>
@@ -106,12 +108,11 @@ export const Footer = () => {
                     <ul className='footer__container__offices__list footer-offices-list'>
                         {
                             offices.map((office, index) =>
-                                <li key={index} className='footer__container__offices__list__item footer-offices-list-item'>
-                                    <Icon className={'footer__container__offices__list__item__icon footer-offices-list-item-icon'}>
-                                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-                                        <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                                <li key={index} className='footer__container__offices__list__item footer-offices-item'>
+                                    <Icon className={'footer__container__offices__list__item__icon footer-offices-icon'}>
+                                        {getIcons('footer', undefined, 'ubication')}
                                     </Icon>
-                                    <span className='footer__container__offices__list__item__text  footer-offices-list-item-text'>
+                                    <span className='footer__container__offices__list__item__text  footer-offices-text'>
                                         {office}
                                     </span>
                                 </li>
